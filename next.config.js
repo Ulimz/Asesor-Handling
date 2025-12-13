31494cd3d9e5bc59701b18086fdc084b765664f2
Ulimz <ulises_sevi@hotmail.com>
2025-12-13 21:03:23 +0100

    fix(build): activar output standalone para docker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export', // Comentado para Railway (SSR)
+    output: 'standalone', // Necesario para Dockerfile.prod
     // trailingSlash: true, // No necesario en SSR
     images: {
         unoptimized: true,
